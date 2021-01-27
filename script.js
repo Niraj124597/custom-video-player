5f9993312acfd68f9d5509fe6073f0961a56e8aa
Niraj Regmi <niraj.regmi005@gmail.com>
2021-01-26 23:53:39 -0800

    removed comments

--- script.js
+++ script.js
@@ -31,8 +31,6 @@ function toggleVolume() {
 }
 
 function updateTime() {
-  // console.log(video.currentTime);
-  // console.log(video.duration); // 208.516 seconds
   progress.value = (video.currentTime / video.duration) * 100;
 
   let mins = Math.floor(video.currentTime / 60);
@@ -42,13 +40,6 @@ function updateTime() {
   secs < 10 ? secs = '0' + String(secs) : null
 
   timestamp.innerText =`${mins}:${secs}`;
-
-  /*
-  progress.value = (video.currentTime / video.duration) * 100;
-  let mins = Math.floor(video.currentTime / 60);
-  let secs = Math.floor(video.currentTime % 60);
-  timestamp.innerText = `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-  */
 }
 
 function setVideoProgress() {
@@ -64,22 +55,10 @@ function toggleScreenSize() {
   video.requestFullscreen();
 }
 
-// function togglePlayIcon() {
-//   if (video.paused) {
-//     video.play();
-//     btnPlay.innerHTML = '<i class="fa fa-pause"></i>';
-//   } else {
-//     video.pause();
-//     btnPlay.innerHTML = '<i class="fa fa-play"></i>';
-//   }
-// }
-
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateTime);
-
-// btnPlay.addEventListener('click', togglePlayIcon);
 
 btnPlay.addEventListener('click', toggleVideoStatus);
 btnStop.addEventListener('click', stopVideo);
